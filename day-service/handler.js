f35a6728aa50ec1624757086bfa6cc660c06b6a2
bladexr8 <bladexr8@gmail.com>
2020-05-02 12:47:12 +1000

    calculateDay Lambda Function

--- day-service/handler.js
+++ day-service/handler.js
@@ -1,14 +1,28 @@
 'use strict';
 
-module.exports.helloWorld = (event, context, callback) => {
+// Helper function
+const getDayFromDate = (dateString) => {
+  const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wendesday', 'Thursday', 'Friday', 'Saturday'];
+  const date = new Date(Date.parse(dateString));
+  const dayIndex = date.getDay();
+  const day = DAYS[dayIndex];
+  return day;
+}
+
+module.exports.calculateDay = (event, context, callback) => {
+  console.log('calculateDay Invoked...');
+  const input = JSON.parse(event.body); // get http body from event
+  console.log(context); // log Lambda Context
+  console.log(event); // log event
+  console.log(input); // log http body
   const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
     },
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      date: input.date,
+      day: getDayFromDate(input.date), // return result
     }),
   };
 
