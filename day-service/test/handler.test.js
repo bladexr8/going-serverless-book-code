6afadeb1e1ee1a6a37ea614bb6018f2c6abeae49
bladexr8 <bladexr8@gmail.com>
2020-05-02 13:25:58 +1000

    Add Lambda Unit Test

--- day-service/test/handler.test.js
+++ day-service/test/handler.test.js
@@ -1,4 +1,18 @@
+const LambdaTester = require('lambda-tester');
+const { calculateDay } = require('../handler');
 
-test('adds 1 + 1 to equal 2', () => {
-    expect((1 + 1)).toBe(2);
+const testEvent = {
+    body: '{ "date": "18 March 1973"}'
+};
+
+describe('handler()', () => {
+    test('returns the correct day of the week', () => {
+        LambdaTester(calculateDay)  // which lambda function to test
+            .event(testEvent)
+            .expectResult((response) => {
+                // Test assertions
+                const data = JSON.parse(response.body);
+                expect(data.day).toBe('Sunday');
+            });
+    });
 });
