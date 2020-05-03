2b4d98ab226acf3c02dfcd8e9f51a3b83560930a
Stephen Moss <bladexr8@gmail.com>
2020-05-03 10:27:32 +1000

    Fixing processImage

--- snapnext/functions/processImage.js
+++ snapnext/functions/processImage.js
@@ -12,22 +12,30 @@ module.exports.handler = (event, context, callback) => {
     console.log('Starting Image Processing...');
     const { facialAnalysis, id } = event.Records[0].dynamodb.NewImage;
     const fileName = id.S;
-    const s3SourceKey = `${process.env.ORIGINAL_FOLDER_NAME}/${fileName}}`;
-    const s3DestinationKey = `${process.env.PROCESSED_FOLDER_NAME}/${fileName}}`;
+    const s3SourceKey = `${process.env.ORIGINAL_FOLDER_NAME}/${fileName}`;
+    const s3DestinationKey = `${process.env.PROCESSED_FOLDER_NAME}/${fileName}`;
+
+    console.log(facialAnalysis);
+    console.log(id);
+    console.log(s3SourceKey);
+    console.log(s3DestinationKey);
 
     // Download an image from S3
     const params = {
         Bucket: process.env.IMAGES_BUCKET_NAME,
         Key: s3SourceKey
     };
+    console.log(params);
     return s3.getObject(params).promise()
         .then((data) => {
+            console.log('Retrieved Source Image from S3...');
             // Write buffer
             const buffer = data.Body;
             // Process Image
             return cropFace(buffer, facialAnalysis);
         })
         .then((outputBuffer) => {
+            console.log('Uploading Target Image to S3...');
             return s3.putObject({
                 Bucket: process.env.IMAGES_BUCKET_NAME,
                 Key: s3DestinationKey,
@@ -43,6 +51,9 @@ module.exports.handler = (event, context, callback) => {
 
 // crop a face, if any
 const cropFace = (buffer, facialAnalysis) => {
+    console.log('Cropping Face...');
+    console.log(buffer);
+    console.log(facialAnalysis);
     return sizeImage(buffer)
         .then((image) => {
             const face = _.get(facialAnalysis, 'M.FaceDetails.L[0].M.BoundingBox.M', false);
@@ -51,19 +62,31 @@ const cropFace = (buffer, facialAnalysis) => {
             const faceX = face.Left.N * image.width;
             const faceY = face.Top.N * image.height;
 
+            console.log('face: ', face);
+            console.log('faceWidth: ', faceWidth);
+            console.log('faceHeight: ', faceHeight);
+            console.log('faceX: ', faceX);
+            console.log('faceY: ', faceY);
+
             // If there is a face in the image, return a crop of the face
-            return (face ? cropImage(buffer, facWidth, faceHeight, faceX, faceY) : buffer);
+            return (face ? cropImage(buffer, faceWidth, faceHeight, faceX, faceY) : buffer);
         });
 };
 
 // Get the width and height of an image
 const sizeImage = (buffer) => {
+    console.log('Resizing Image...');
     return new Promise((resolve, reject) => {
         gm(buffer)
             .size((err, dimensions) => {
+                console.log('sizeImage callback...');
                 if (err) {
+                    console.log('sizeImage callback err...');
+                    console.log(err);
                     reject(err);
                 } else {
+                    console.log('sizeImage callback resolve...');
+                    console.log(dimensions);
                     resolve(dimensions);
                 }
             });
@@ -72,6 +95,7 @@ const sizeImage = (buffer) => {
 
 // Crops an image starting from a given x and y coordinate
 const cropImage = (buffer, width, height, x, y) => {
+    console.log('Cropping Image...');
     return new Promise((resolve, reject) => {
         gm(buffer)
             .crop(width, height, x, y)
